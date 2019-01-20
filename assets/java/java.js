1dafba4a1ce76e10c09da97dfdb3690848b791e5
jeckhart582@gmail.com <jeckhart582@gmail.com>
2019-01-20 17:42:47 -0600

    Third push, .js and firebase up and running

---
+++ assets/java/java.js
@@ -0,0 +1,66 @@
+ // Initialize Firebase
+ var config = {
+    apiKey: "AIzaSyDzuPT3g9quIGpd3Xu-XyWgIczlldTi1IY",
+    authDomain: "train-firebase-dd3f5.firebaseapp.com",
+    databaseURL: "https://train-firebase-dd3f5.firebaseio.com",
+    projectId: "train-firebase-dd3f5",
+    storageBucket: "train-firebase-dd3f5.appspot.com",
+    messagingSenderId: "273265755739"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+  $("#userConfirm").on("click", function (){
+
+    var userTrainName = $("#userTrainName").val().trim();
+    var userDestination = $("#userDestination").val().trim();
+    var userTime = moment($("#userTime").val().trim(), "HH:mm").format ("HH:mm");
+    var userFrequency = $("#userFrequency").val().trim();
+
+    var userTrain = {
+        name: userTrainName,
+        destination: userDestination,
+        inputTime: userTime,
+        frequency: userFrequency,
+    }
+    database.ref().push(userTrain);
+    console.log(userTrain);
+
+    $("userTrainName").empty();
+    $("userDestination").empty();
+    $("userTime").empty();
+    $("userFrequency").empty();
+
+    return false;
+
+  });
+
+  database.ref().on("child_added", function(childSnapshot){
+      console.log(childSnapshot.val());
+
+      var trainName = childSnapshot.val().name;
+      var destination = childSnapshot.val().destination;
+      var time = childSnapshot.val().inputTime;
+      var frequency = childSnapshot.val().frequency;
+
+      var timeConvert = moment(time, "HH:mm");
+      var currentTime = moment().format("HH:mm");
+      console.log (currentTime);
+
+      var mathTime = moment().diff(moment(timeConvert), "minutes");
+      console.log(mathTime);
+    
+      var timeDivide = mathTime % frequency;
+      console.log (timeDivide)
+
+      var nextTrainTime = frequency - timeDivide;
+      console.log(nextTrainTime)
+
+      var nextTrain = moment().add(nextTrainTime, "minutes").format("HH:mm");
+      console.log(nextTrain)
+
+      $("#trainTable>tbody").append("<tr><td>" + trainName + "</td><td>" + destination + "</td><td>" + frequency + "</td><td>" + nextTrain+ "</td><td>" + nextTrainTime + "</td></tr>");
+
+      
+  })
